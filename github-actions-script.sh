b4a7e5ed23b2994f8752168f383e94ede6831a23
Awadh Prajapat <awadh.prajapat@infobeans.com>
2022-08-08 12:48:53 +0530

    10 steps

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -1,8 +1,24 @@
-ls -a 
+echo 1. What is in the directory?
+ls -a
 echo
-echo is java installed
+echo 2. Is java installed?
 java --version
-echo is git installed
+echo 
+echo 3. Is git installed?
 git --version
-echo what environment variable is available
+echo
+echo 5. Where is android SDK root?
+echo $ANDROID_SDK_ROOT
+echo
+echo 6. Where are the selenium jars
+echo $SELENIUM_JAR_PATH
+echo
+echo 7. What is the workspace location
+echo $RUNNER_WORKSPACE
+echo
+echo 8. Who is running this script
+whoami
+echo 9. How is disc laid out?
+df
+echo 10. What environment variables are available
 env
